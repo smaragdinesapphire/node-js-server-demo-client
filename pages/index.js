d98a7d931aec25dac706a1bc56ff04795305c155
KingJ <smaragdinesapphire@gmail.com>
2023-07-25 17:12:33 +0800

    feat: 打後端讀寫資料，包含用 WS 與 AXIOS

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,63 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useCallback, useEffect, useRef } from "react";
+import { Button } from "antd";
+import { Space } from "antd";
+import axios from "axios";
+import { notification } from "antd";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
+const SEND_DATA = { name: "John", age: 30 };
 export default function Home() {
+  const wsRef = useRef();
+
+  const handleSaveByWS = useCallback(() => {
+    wsRef.current.send(JSON.stringify({ type: "save", data: SEND_DATA }));
+  }, []);
+
+  const handleLoadByWS = useCallback(() => {
+    wsRef.current.send(JSON.stringify({ type: "load" }));
+  }, []);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+
+    ws.onopen = () => {
+      console.log("WebSocket connection opened.");
+    };
+
+    ws.onmessage = (event) => {
+      console.log("Received message from server:", event.data);
+    };
+
+    ws.onclose = () => {
+      console.log("WebSocket connection closed.");
+    };
+    wsRef.current = ws;
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  const handleSaveByAxios = useCallback(() => {
+    axios
+      .post("http://localhost:8080/save", SEND_DATA)
+      .then(() => notification.success())
+      .catch(() => notification.error());
+  }, []);
+
+  const handleLoadByAxios = useCallback(
+    () =>
+      axios
+        .get("http://localhost:8080/load")
+        .then(({ data }) => console.log("success", data))
+        .catch((e) => notification.error()),
+    []
+  );
+
   return (
     <>
       <Head>
@@ -15,100 +67,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <Space>
+          <span>WebSocket</span>
+          <Button onClick={() => handleSaveByWS()}>Save</Button>
+          <Button onClick={() => handleLoadByWS()}>Load</Button>
+        </Space>
+        <Space>
+          <span>Axios</span>
+          <Button onClick={() => handleSaveByAxios()}>Save</Button>
+          <Button onClick={() => handleLoadByAxios()}>Load</Button>
+        </Space>
       </main>
     </>
-  )
+  );
 }
